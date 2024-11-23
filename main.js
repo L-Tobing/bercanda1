91363871295031a61c08de87a8b12e82c18db2ec
L-Tobing <mrasidin55@gmail.com>
2024-11-23 17:43:42 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+var nama = prompt("masukan nama");
+alert("halo" + nama);
+alert(nama + "maaf iyh ganggu waktu nya aku mau ngomong sesuatu");
+var lagi = false;
+while (lagi === false) {
+  confirm("kamu yakin");
+  var tes = confirm("mau kah kamu menjadi bagian dalam ceritaku");
+  if (tes === true) {
+    alert("terimakasih iyh udah mau jadi bagian dalam hidup ku" + nama);
+  }
+}
